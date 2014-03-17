1e766bbc18b19f83c1c7128a095d371ebad2c732
Matthew the Ichiban Gaijin <ichibangaijin77@gmail.com>
2014-03-17 15:02:11 -0700

    first draft of completed project

--- js/app.js
+++ js/app.js
@@ -1,10 +1,51 @@
-$(document).ready(function(){
-	$('.ryu').mouseenter(function(){
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
+$(document).ready(function () {
+		$('.ryu').mouseenter(function () {
+			$('.ryu-still').hide();
+			$('.ryu-ready').show();
 		})
-	.mouseleave(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-still').show()
-	});
+			.mouseleave(function () {
+				$('.ryu-ready').hide();
+				$('.ryu-still').show();
+			})
+			.mousedown(function () {
+				playHadouken();
+					$('.ryu-ready').hide();
+					$('.ryu-throwing').show();
+					$('.hadouken').show();
+						$('.hadouken').animate(
+							{'left': '1100px'},
+							800,
+							function() {
+								$(this).hide();
+								$(this).css('left', '600px');
+								$('.ryu-throwing').hide();
+								$('.ryu-ready').show();
+							}
+										
+						);
+					})
+		
+		});
+
+		function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+
+
+$(document).keydown(function(e){
+	if (e.keyCode == 88) {
+	$('.ryu-ready').hide();
+	$('.ryu-throwing').hide();
+	$('.ryu-cool').show();
+	}
 });
+	
+	
+$(document).keyup(function(e){
+	if (e.keyCode == 88){
+		$('.ryu-ready').show();
+			$('.ryu-cool').hide();
+		}
+	});
